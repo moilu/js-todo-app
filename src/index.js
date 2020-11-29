61791aa0831b5066d604bfbd5937d89b51c23abe
Moilu <moisesluna0390@gmail.com>
2020-11-29 13:50:53 -0800

    [FINISHED]javascript todo app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import { Todo } from './classes';
-
+import { TodoList } from './classes/todo-list.class';
+import { crearToDoHtml } from './js/componentes.js';
 import './styles.css';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( crearToDoHtml );
+
+console.log( todoList );
